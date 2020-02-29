e905f88cae05edc8fc55543244371f00c3417511
Henry Larson <hxl1116@g.rit.edu>
2020-02-29 10:19:10 -0500

    Added 'predeploy' script.

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
         index: './index'
     },
     output: {
-        path: resolve(__dirname, './dist'),
+        path: resolve(__dirname, 'dist'),
         filename: '[name].bundle.js',
         // publicPath: '/'
     },
